019ab26b494258dc96dfb29ca4af0acf170b96dc
Pratik06m <pratikpatil2872@gmail.com>
2024-12-06 19:05:54 +0530

    Pairs Array

--- Arrays/PairsArray.java
+++ Arrays/PairsArray.java
@@ -1,23 +1,23 @@
 import java.util.*;
 
 public class PairsArray {
-    public static void Pairs(int numbers[]){
+    public static void Pairs(int array[]){
         int totalPairs = 0;
-        for (int i =0; i< numbers.length; i++){
-            int curr = numbers[i];
-            for(int j = i+1; j<numbers.length;j++){
-                System.out.print("(" + curr + ","+ numbers[j] + ") ");
+        for (int i =0; i< array.length; i++){
+            int curr = array[i];
+            for(int j = i+1; j<array.length;j++){
+                System.out.print("(" + curr + ","+ array[j] + ") ");
                 totalPairs++;
             }
             System.out.println();
         }
         System.out.println("Total Pairs is: "+ totalPairs); 
         // Formula n*(n-1)/2
-        System.out.println(numbers.length*(numbers.length - 1)/2);
+        System.out.println(array.length*(array.length - 1)/2);
 
     }
     public static void main(String[] args) {
-        int numbers[] = {2,4,6,8,10,12,14,16,18,20};
-        Pairs(numbers);
+        int array[] = {2,4,6,8,10,12,14,16,18,20};
+        Pairs(array);
     }
 }
