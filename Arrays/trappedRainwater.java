cd7bc394825da30cbcfca61aae8be12d4e7cc20b
Pratik06m <pratikpatil2872@gmail.com>
2024-12-10 17:07:16 +0530

    Buying selling stocks problem

--- Arrays/trappedRainwater.java
+++ Arrays/trappedRainwater.java
@@ -33,8 +33,8 @@ public class trappedRainwater {
         }
         return trappedwater;
     }
-    public static void main(String[] args) {
-        int height[] = {4,2,0,6,3,2,5};
+     public static void main(String[] args) {
+        int height[] = {0,1,0,2,1,0,1,3,2,1,2,1};
         System.out.println(rainwater(height));
     }
 }
