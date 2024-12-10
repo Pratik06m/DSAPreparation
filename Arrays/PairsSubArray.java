ae11caa15d7399fddd6319a8c39748df5258fa7a
Pratik06m <pratikpatil2872@gmail.com>
2024-12-10 16:22:27 +0530

    Trapped rainwater Solved

--- Arrays/PairsSubArray.java
+++ Arrays/PairsSubArray.java
@@ -56,10 +56,25 @@ public class PairsSubArray {
         }
         System.out.println("Max Sum: "+maxSum);
     }
+    
+    public static void KadanesSum(int numbers[]){
+        int ms = Integer.MIN_VALUE;
+        int cs = 0;
+
+        for(int i=0; i<numbers.length;i++){
+            cs = cs + numbers[i];
+            if(cs < 0){
+                cs =0;
+            }
+            ms = Math.max(cs, ms);
+        }
+        System.out.println("Maximum subarray sum is: "+ms);
+    }
     public static void main(String[] args) {
-        int numbers[] = {2,4,6,8,10,12,14,16,18,20};
+        int numbers[] = {2,-4,6,-8,10,-12,14,-16,18,-20};
         // subArray(array);
         // maxsubArraySum(array);
         prefixArraySum(numbers);
+        KadanesSum(numbers);
     }
 }
---
+++ Arrays/trappedRainwater.java
@@ -0,0 +1,40 @@
+public class trappedRainwater {
+
+    public static int rainwater(int height[]){
+        int n = height.length;
+
+        //calculate left maximum height 
+        int leftMax[] = new int[n];
+        leftMax[0] = height[0];
+        for(int i=1; i<n;i++){
+            leftMax[i] = Math.max(height[i], leftMax[i-1]);
+        }
+
+        //calculate right maximum height
+        int rightMax[] = new int[n];
+        rightMax[n-1] = height[n-1];
+        for(int i = n-2; i>=0;i--){
+            rightMax[i] = Math.max(height[i], rightMax[i+1]);
+        }
+
+        int trappedwater = 0;
+        
+        //Loop for traverse the array of height
+
+        for(int i =0;i<n;i++){
+
+            //waterLevel = min(leftMax , rightMax)
+
+            int waterLevel = Math.min(leftMax[i], rightMax[i]);
+
+            //trappedwater = waterlevel - height of bar height[i] and add that water in trappedwater
+
+            trappedwater += waterLevel - height[i];
+        }
+        return trappedwater;
+    }
+    public static void main(String[] args) {
+        int height[] = {4,2,0,6,3,2,5};
+        System.out.println(rainwater(height));
+    }
+}
