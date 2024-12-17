631dd4acad6fecf7a4b5636bb1d0f9343db3fcae
Pratik06m <pratikpatil2872@gmail.com>
2024-12-17 13:40:27 +0530

    Update Ith Bit

--- Bit_Manipulation/OperationIthBit.java
+++ Bit_Manipulation/OperationIthBit.java
@@ -16,9 +16,33 @@ public class OperationIthBit {
          return n | bitMask;
     
     }
+
+    public static int clearithbit(int n, int i){
+        int bitMask = ~(1<<i);
+         return n & bitMask;
+    
+    }
+
+    public static int updateithbit(int n, int i, int newBit){
+
+        //First Way
+        
+        // if(newBit ==0){
+        //     return clearithbit(n, i);
+        // }
+        // else{
+        //     return setithbit(n, i);
+        // }
+
+        //Second way
+
+        n = clearithbit(n, i);
+        int bitMask = newBit<<i;
+        return n |bitMask;
+    }
     public static void main(String[] args) {
         System.out.println(getithbit(10, 2));
         System.out.println(setithbit(10, 2));
-
+        System.out.println(clearithbit(10, 1));
     }    
 }
