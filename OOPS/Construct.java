c6925557dce0c23ab69eef346efdac4b55453775
Pratik06m <pratikpatil2872@gmail.com>
2024-12-24 14:56:04 +0530

    Types of Constructor

--- OOPS/Construct.java
+++ OOPS/Construct.java
@@ -1,14 +1,27 @@
 public class Construct {
     public static void main(String[] args) {
-        Student s1 = new Student("Pratik");
-        System.out.println(s1.name);
+        Student s1 = new Student();
+        Student s2 = new Student("Pratik");
+        Student s3 = new Student(123);
+
+        System.out.println(s2.name);
+        System.out.println(s3.rollno);
+
+        
     }
 }
 
 class Student{
     String name;
-    
+    int rollno;
+
+    Student(){
+        System.out.println("Jay shree Ram");
+    }
     Student(String name){
         this.name = name;
+    }
+    Student (int rollno){
+        this.rollno = rollno;
     }
 }
