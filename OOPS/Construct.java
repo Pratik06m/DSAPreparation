51a08d85df5295554f7a4de98c42437351d28cde
Pratik06m <pratikpatil2872@gmail.com>
2024-12-24 15:16:36 +0530

    Copy Constructor

--- OOPS/Construct.java
+++ OOPS/Construct.java
@@ -1,11 +1,21 @@
+import javax.management.remote.SubjectDelegationPermission;
+
 public class Construct {
     public static void main(String[] args) {
         Student s1 = new Student();
         Student s2 = new Student("Pratik");
         Student s3 = new Student(123);
-
+        
+        
+        Student s4 = new Student(s2);   //Copy Connstructor
+        System.out.println(s4.name);
+        
+        
         System.out.println(s2.name);
         System.out.println(s3.rollno);
+
+
+
 
         
     }
@@ -14,14 +24,21 @@ public class Construct {
 class Student{
     String name;
     int rollno;
+    String pass;
+    int marks[]; 
 
-    Student(){
+    Student(Student s2){    //Copy Connstructor
+        this.name = s2.name;
+    }
+
+
+    Student(){          // Non parameterized
         System.out.println("Jay shree Ram");
     }
-    Student(String name){
+    Student(String name){   // Parameterized
         this.name = name;
     }
-    Student (int rollno){
+    Student (int rollno){   
         this.rollno = rollno;
     }
 }
