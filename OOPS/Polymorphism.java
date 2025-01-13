7e718d24273f625eda1ea47c0dd5c390eb2295fc
Pratik06m <pratikpatil2872@gmail.com>
2025-01-13 15:13:29 +0530

    Compile time Polymorphism

---
+++ OOPS/Polymorphism.java
@@ -0,0 +1,23 @@
+public class Polymorphism {
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.sum(10, 20));
+        System.out.println(calc.sum((float)10.10, (float) 20.50));
+
+        System.out.println(calc.sum(10, 20, 30));
+
+    }
+}
+class Calculator{
+    int sum(int a, int b){
+        return a+b;
+    }
+
+    float sum (float a, float b){
+        return a+b;
+    }
+    
+    int sum(int a, int b, int c){
+        return a+b+c;
+    }
+}
